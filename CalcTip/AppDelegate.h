f880c6dc427e0d23283a5e88713ad3fa0244dc84
Monica Bui <monicahb@fb.com>
2020-06-23 15:02:44 -0700

    Initial Commit

---
+++ CalcTip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalcTip
+//
+//  Created by Monica Bui on 6/23/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CalcTip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalcTip
+//
+//  Created by Monica Bui on 6/23/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
