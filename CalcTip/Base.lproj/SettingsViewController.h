ab9a9ec08bcbfd02f38e365d114163b832c09006
Monica Bui <monicahb@fb.com>
2020-06-23 23:17:27 -0700

    Added settings button to nav bar

---
+++ CalcTip/Base.lproj/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  CalcTip
+//
+//  Created by Monica Bui on 6/23/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
